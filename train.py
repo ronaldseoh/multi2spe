5e1de044e3daba30720aac8036eb87790700de67
Ronald Seoh <ronaldseoh@icloud.com>
2021-06-28 18:46:29 -0400

    train.py: calculate average distance between each embeddings and their center points; add these distances to the logger

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ class MultiFacetTripletLoss(torch.nn.Module):
 
                 distance_positive = torch.min(distance_positive_all, dim=1).values
                 distance_negative = torch.min(distance_negative_all, dim=1).values
-                
+
             losses = torch.nn.functional.relu(distance_positive - distance_negative + self.margin)
         elif self.distance == 'cosine':  # independent of length
             distance_positive = torch.nn.functional.cosine_similarity(query, positive)
@@ -218,6 +218,28 @@ class QuarterMaster(pl.LightningModule):
             source_embedding = self.model(**batch[0]).last_hidden_state[:, 0:self.hparams.num_facets, :]
             pos_embedding = self.model(**batch[1]).last_hidden_state[:, 0:self.hparams.num_facets, :]
             neg_embedding = self.model(**batch[2]).last_hidden_state[:, 0:self.hparams.num_facets, :]
+
+            if self.hparams.num_facets > 1:
+                source_center_point = torch.mean(source_embedding, 1, keepdims=True)
+                source_embedding_distances = torch.cdist(source_center_point, source_embedding, p=2)
+
+                pos_center_point = torch.mean(pos_embedding, 1, keepdims=True)
+                pos_embedding_distances = torch.cdist(pos_center_point, pos_embedding, p=2)
+
+                neg_center_point = torch.mean(neg_embedding, 1, keepdims=True)
+                neg_embedding_distances = torch.cdist(neg_center_point, neg_embedding, p=2)
+
+                self.log(
+                    'avg_facet_dist_source', torch.mean(source_embedding_distances),
+                    on_step=True, on_epoch=False, prog_bar=True, logger=True)
+
+                self.log(
+                    'avg_facet_dist_pos', torch.mean(pos_embedding_distances),
+                    on_step=True, on_epoch=False, prog_bar=True, logger=True)
+
+                self.log(
+                    'avg_facet_dist_neg', torch.mean(neg_embedding_distances),
+                    on_step=True, on_epoch=False, prog_bar=True, logger=True)
 
         loss = self.loss(source_embedding, pos_embedding, neg_embedding)
 
