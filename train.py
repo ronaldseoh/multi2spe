7ee1c9ca0b665ac73f4f9de7a38c5d4244b73706
Ronald Seoh <ronaldseoh@icloud.com>
2021-07-29 16:40:21 -0400

    make pos_embedding pass through extra_facet_layers_for_target too

--- train.py
+++ train.py
@@ -243,14 +243,15 @@ class QuarterMaster(pl.LightningModule):
             if len(self.extra_facet_layers) > 0:
                 for n in range(self.hparams.num_facets):
                     source_embedding[:, n, :] = self.extra_facet_layers[n](source_embedding[:, n, :])
-                    pos_embedding[:, n, :] = self.extra_facet_layers[n](pos_embedding[:, n, :])
 
             # For negative papers, we could choose to train separate linear layers from source/pos
             if len(self.extra_facet_layers_for_target) > 0:
                 for n in range(self.hparams.num_facets):
+                    pos_embedding[:, n, :] = self.extra_facet_layers_for_target[n](pos_embedding[:, n, :])
                     neg_embedding[:, n, :] = self.extra_facet_layers_for_target[n](neg_embedding[:, n, :])
             elif len(self.extra_facet_layers) > 0:
                 for n in range(self.hparams.num_facets):
+                    pos_embedding[:, n, :] = self.extra_facet_layers[n](pos_embedding[:, n, :])
                     neg_embedding[:, n, :] = self.extra_facet_layers[n](neg_embedding[:, n, :])
 
         loss = self.loss(source_embedding, pos_embedding, neg_embedding)
@@ -325,14 +326,15 @@ class QuarterMaster(pl.LightningModule):
             if len(self.extra_facet_layers) > 0:
                 for n in range(self.hparams.num_facets):
                     source_embedding[:, n, :] = self.extra_facet_layers[n](source_embedding[:, n, :])
-                    pos_embedding[:, n, :] = self.extra_facet_layers[n](pos_embedding[:, n, :])
 
             # For negative papers, we could choose to train separate linear layers from source/pos
             if len(self.extra_facet_layers_for_target) > 0:
                 for n in range(self.hparams.num_facets):
+                    pos_embedding[:, n, :] = self.extra_facet_layers_for_target[n](pos_embedding[:, n, :])
                     neg_embedding[:, n, :] = self.extra_facet_layers_for_target[n](neg_embedding[:, n, :])
             elif len(self.extra_facet_layers) > 0:
                 for n in range(self.hparams.num_facets):
+                    pos_embedding[:, n, :] = self.extra_facet_layers[n](pos_embedding[:, n, :])
                     neg_embedding[:, n, :] = self.extra_facet_layers[n](neg_embedding[:, n, :])
 
         # Normalize each facet embeddings
