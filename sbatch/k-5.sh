09c9786be62a3224a7781f447117357ce13367a0
Ronald Seoh <ronaldseoh@icloud.com>
2021-08-24 00:56:41 -0400

    sbatch: switch back to 2080ti as default partiton

--- sbatch/k-5.sh
+++ sbatch/k-5.sh
@@ -3,7 +3,7 @@
 #SBATCH -o sbatch_logs/stdout/k-5_%j.txt
 #SBATCH -e sbatch_logs/stderr/k-5_%j.err
 #SBATCH --ntasks=1
-#SBATCH --partition=1080ti-long
+#SBATCH --partition=2080ti-long
 #SBATCH --gres=gpu:1
 #SBATCH --mem=24GB
 #SBATCH --cpus-per-task=2
