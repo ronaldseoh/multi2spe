bed3fe17be7fefccceb4b7e818c35b59350f9935
Ronald Seoh <ronaldseoh@icloud.com>
2021-09-09 13:40:54 -0400

    snippets/shuffle_specter_pickled_dataset.py: minor

--- snippets/shuffle_specter_pickled_dataset.py
+++ snippets/shuffle_specter_pickled_dataset.py
@@ -9,11 +9,10 @@ import tqdm
 # https://stackoverflow.com/questions/45808140/using-tqdm-progress-bar-in-a-while-loop
 # for how to use tqdm with infinite loops
 def unpickle_instances():
-    while True:
-        try:
-            yield unpickler.load()
-        except EOFError:
-            break
+    try:
+        yield unpickler.load()
+    except EOFError:
+        break
 
 
 if __name__ == "__main__":
