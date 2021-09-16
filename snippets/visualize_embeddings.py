ddaa6978ad2472d72041aac0b046ef57d5ac0706
Ronald Seoh <ronaldseoh@icloud.com>
2021-09-16 15:55:40 -0400

    snippets/visualize_embeddings.py

--- snippets/visualize_embeddings.py
+++ snippets/visualize_embeddings.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     # Read the embeddings jsonl created with embed.py
     mag_embeddings = []
     facet_labels = []
+    mag_labels = []
 
     with open("quartermaster/save_k-3_common_8_4_identical_random_09-12/cls.jsonl", "r") as mag_embeddings_file:
         for line in tqdm.tqdm(mag_embeddings_file):
@@ -27,16 +28,24 @@ if __name__ == "__main__":
                 for i, emb in enumerate(paper["embedding"]):
                     mag_embeddings.append(np.array(emb))
                     facet_labels.append(i)
+                    mag_labels.append(mag_val[mag_val.pid == paper["paper_id"]].iloc[0].class_label)
 
     # Try reducing all embeddings and color code then by the facet #
     mag_embeddings = np.array(mag_embeddings)
     facet_labels = np.array(facet_labels)
+    mag_labels = np.array(mag_labels)
 
     # Run UMAP
     mapper = umap.UMAP().fit(mag_embeddings)
 
-    # Plot
+    # Plot first with facet #
     umap.plot.points(mapper, labels=facet_labels)
 
     # Save the plot to a file
-    plt.savefig("plot.png")
+    plt.savefig("plot1.png")
+    
+    # Then plot with MAG labels
+    umap.plot.points(mapper, labels=mag_labels)    
+
+    # Save the plot to a file
+    plt.savefig("plot2.png")
