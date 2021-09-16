4ab011821131c7f0fb7553c0a6500ed9a4be912a
Ronald Seoh <ronaldseoh@icloud.com>
2021-09-16 19:43:36 -0400

    Update visualize_embeddings_cs_titles.py

--- snippets/visualize_embeddings_cs_titles.py
+++ snippets/visualize_embeddings_cs_titles.py
@@ -12,14 +12,35 @@ NUM_FACETS = 3
 
 K = 5
 
+mag_label_mapping = {
+    0: "Art",
+    1: "Biology",
+    2: "Business",
+    3: "Chemistry",
+    4: "Computer science",
+    5: "Economics",
+    6: "Engineering",
+    7: "Environmental science",
+    8: "Geography",
+    9: "Geology",
+    10:	"History",
+    11: "Materials science",
+    12: "Mathematics",
+    13:	"Medicine",
+    14: "Philosophy",
+    15: "Physics",
+    16: "Political science",
+    17: "Psychology",
+    18: "Sociology",
+}
+
 if __name__ == "__main__":
     # Load the paper ids in the MAG validation set
     mag_val = pd.read_csv("scidocs/data/mag/val.csv")
+    mag_val_pids = set(mag_val.pid)
 
     # Pick computer science papers only using the label=4
-    mag_val_cs = mag_val[mag_val.class_label == 4]
-
-    mag_val_pids = set(mag_val_cs.pid)
+    mag_val_cs_indexes = mag_val[mag_val.class_label == 4].index.to_list()
 
     # MAG metadata to get the paper titles
     mag_metadata = json.load(open("scidocs/data/paper_metadata_mag_mesh.json", "r"))
@@ -31,6 +52,7 @@ if __name__ == "__main__":
         mag_embeddings_by_facets[f] = []
 
     mag_titles = []
+    mag_labels = []
 
     with open("quartermaster/save_k-3_common_8_4_identical_random_09-12/cls.jsonl", "r") as mag_embeddings_file:
         for line in tqdm.tqdm(mag_embeddings_file):
@@ -40,6 +62,7 @@ if __name__ == "__main__":
                 for f, emb in enumerate(paper["embedding"]):
                     mag_embeddings_by_facets[f].append(np.array(emb))
                 mag_titles.append(mag_metadata[paper["paper_id"]]["title"])
+                mag_labels.append(mag_val[mag_val.pid == paper["paper_id"]].iloc[0].class_label)
 
     for f in range(NUM_FACETS):
         mag_embeddings_by_facets[f] = np.array(mag_embeddings_by_facets[f])
@@ -47,7 +70,7 @@ if __name__ == "__main__":
     cosine_similarities_by_facets = {}
     search_results_by_facets = {}
 
-    sample_idxs = random.sample(range(len(mag_titles)), k=10)
+    sample_idxs = random.sample(mag_val_cs_indexes, k=10)
 
     for f in range(NUM_FACETS):
         cosine_similarities_by_facets[f] = sklearn.metrics.pairwise.euclidean_distances(
@@ -70,6 +93,7 @@ if __name__ == "__main__":
                 # Iterate through top 5 papers from the search results for this facet
                 for j in range(K):
                     titles_file.write(mag_titles[search_results_by_facets[f][i][j]])
+                    titles_file.write(" (" + mag_label_mapping[mag_labels[search_results_by_facets[f][i][j]]] + ")")
                     titles_file.write("\n")
 
                 titles_file.write("\n")
