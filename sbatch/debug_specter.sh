11922445cbcc5bf2005bce3c12e505f463381f93
Ronald Seoh <ronaldseoh@icloud.com>
2022-01-15 14:25:01 -0500

    Create debug_specter.sh

---
+++ sbatch/debug_specter.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --job-name=debug_specter
+#SBATCH -o sbatch_logs/stdout/debug_specter_%j.txt
+#SBATCH -e sbatch_logs/stderr/debug_specter_%j.err
+#SBATCH --ntasks=1
+#SBATCH --partition=2080ti-long
+#SBATCH --gres=gpu:1
+#SBATCH --mem=24GB
+#SBATCH --cpus-per-task=2
+
+eval "$(conda shell.bash hook)"
+conda activate qm
+
+EXPERIMENT_ID_PREFIX=debug_specter
+EXPERIMENT_DATE=`date +"%m-%d"`
+
+python train.py --save_dir save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE} \
+                --train_file ~/original_data/train_shuffled.pkl --train_size 684100 \
+                --val_file ~/original_data/val_shuffled.pkl --val_size 145375 \
+                --model_behavior 'specter' \
+                --gpus 1 --num_workers 0 --fp16 \
+                --batch_size 2 --grad_accum 16  --num_epochs 2 \
+                --seed 1945 \
+                --wandb
+
+python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
+                --data-path ../scidocs/data/paper_metadata_mag_mesh.json \
+                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/cls.jsonl --batch-size 4
+                
+python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
+                --data-path ../scidocs/data/paper_metadata_recomm.json \
+                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/recomm.jsonl --batch-size 4
+                
+python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
+                --data-path ../scidocs/data/paper_metadata_view_cite_read.json \
+                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation.jsonl --batch-size 4
+
+conda deactivate
+conda activate scidocs
+
+python ../scidocs/scripts/run.py --cls ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/cls.jsonl \
+                      --user-citation ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation.jsonl \
+                      --recomm ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/recomm.jsonl \
+                      --val_or_test test \
+                      --multifacet-behavior extra_linear \
+                      --n-jobs 4 --cuda-device 0 \
+                      --cls-svm \
+                      --data-path ../scidocs/data \
+                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results.xlsx
