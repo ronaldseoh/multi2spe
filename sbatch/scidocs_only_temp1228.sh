a25a83f6270ddd926e21850ea205c44e16ef090f
Ronald Seoh <ronaldseoh@icloud.com>
2022-01-18 19:41:48 -0500

    Update scidocs_only_temp1228.sh

--- sbatch/scidocs_only_temp1228.sh
+++ sbatch/scidocs_only_temp1228.sh
@@ -9,24 +9,12 @@
 #SBATCH --cpus-per-task=2
 
 eval "$(conda shell.bash hook)"
-conda activate qm
+
 
 EXPERIMENT_ID_PREFIX=U_k-3_sum_embs_original-0-9+no_sum-0-1+max-kmeans_word-0-05_extra_facet_alternate_layer_8_4_identity_common_random_cross_entropy
 EXPERIMENT_DATE="01-17"
 
-python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
-                --data-path ../scidocs/data/paper_metadata_mag_mesh.json \
-                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/cls.jsonl --batch-size 4
-                
-python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
-                --data-path ../scidocs/data/paper_metadata_recomm.json \
-                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/recomm.jsonl --batch-size 4
-                
-python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
-                --data-path ../scidocs/data/paper_metadata_view_cite_read.json \
-                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation.jsonl --batch-size 4
 
-conda deactivate
 conda activate scidocs
 
 python ../scidocs/scripts/run.py --cls save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/cls.jsonl \
