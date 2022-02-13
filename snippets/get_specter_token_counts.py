b78d3854bd4a955be47f78fec68491738eec3b50
Ronald Seoh <ronaldseoh@icloud.com>
2022-02-13 11:32:07 -0500

    Update get_specter_token_counts.py

--- snippets/get_specter_token_counts.py
+++ snippets/get_specter_token_counts.py
@@ -1,3 +1,6 @@
+from train import QuarterMaster
+import utils
+
 import argparse
 import collections
 import json
@@ -5,9 +8,6 @@ import json
 import torch
 import transformers
 import tqdm
-
-from train import QuarterMaster
-import utils
 
 
 if __name__ == '__main__':
