b6bd0bf17e91f301c18c45642101c84c8905ed0e
Ronald Seoh <ronaldseoh@icloud.com>
2022-03-13 04:20:28 -0400

    Update custom_bert.py

--- custom_bert.py
+++ custom_bert.py
@@ -213,12 +213,12 @@ class BertModelWithExtraLinearLayersForMultiFacets(transformers.BertModel):
 
         extended_attention_mask = super().get_extended_attention_mask(attention_mask, input_shape, device)
         extended_attention_mask = torch.repeat_interleave(extended_attention_mask, input_shape[1], dim=2)
-        extended_attention_mask = extended_attention_mask.to(dtype=self.dtype)
+        extended_attention_mask = extended_attention_mask.to(device, dtype=self.dtype)
 
         if abs(self.adjust_attention_mask_for_facets) > 0:
             additional_attention_mask_weights = self.adjust_attention_mask_for_facets * torch.ones((self.num_facets, self.num_facets))
             additional_attention_mask_weights.fill_diagonal_(0)
-            additional_attention_mask_weights = additional_attention_mask_weights.to(dtype=self.dtype)
+            additional_attention_mask_weights = additional_attention_mask_weights.to(device, dtype=self.dtype)
 
             extended_attention_mask[:, :, :self.num_facets, :self.num_facets] += additional_attention_mask_weights.expand_as(extended_attention_mask[:, :, :self.num_facets, :self.num_facets])
 
