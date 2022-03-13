03b1facb574a3041568d0cb40b951884d9cdeb5a
Ronald Seoh <ronaldseoh@icloud.com>
2022-03-13 06:06:04 -0400

    Update k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_drop_facets_randomly_identity_common_random_cross_entropy.sh

--- sbatch/unity/k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_drop_facets_randomly_identity_common_random_cross_entropy.sh
+++ sbatch/unity/k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_drop_facets_randomly_identity_common_random_cross_entropy.sh
@@ -26,7 +26,7 @@ python train.py --save_dir save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE} \
                 --add_extra_facet_nonlinearity \
                 --add_extra_facet_layers_alternate \
                 --add_bert_layer_facet_layers_alternate \
-                ----train_drop_facets_randomly \
+                --train_drop_facets_randomly \
                 --loss_config '[{"name": "original", "weight": 0.9, "loss_type": "bce", "margin": 1.0, "distance": "dot", "reduction": "mean", "reduction_multifacet": "max", "use_target_token_embs": false, "sum_into_single_embeddings": true}, {"name": "no_sum", "weight": 0.1, "loss_type": "bce", "margin": 1.0, "distance": "dot", "reduction": "mean", "reduction_multifacet": "max", "use_target_token_embs": false, "sum_into_single_embeddings": false}, {"name": "mean_and_word_emb", "weight": 0.05, "loss_type": "bce", "margin": 1.0, "distance": "dot", "reduction": "mean", "reduction_multifacet": "mean", "use_target_token_embs": true}]' \
                 --gpus 1 --num_workers 0 --fp16 \
                 --batch_size 2 --grad_accum 16  --num_epochs 2 \
