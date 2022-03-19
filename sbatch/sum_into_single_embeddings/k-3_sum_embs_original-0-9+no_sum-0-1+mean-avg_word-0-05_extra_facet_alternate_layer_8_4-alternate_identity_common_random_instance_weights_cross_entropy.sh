e5403fe34bd4e6217fb6992c629313ee9a5d78a5
Ronald Seoh <ronaldseoh@icloud.com>
2022-03-19 08:18:55 -0400

    Revert "train.py: try giving pos/neg instance weights to word emb losses only"
    This reverts commit 4aa092f241f1f7928a9e31af1d2b13519e2a30ee.

--- sbatch/sum_into_single_embeddings/k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy.sh
+++ sbatch/sum_into_single_embeddings/k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-3_cross_entropy
-#SBATCH -o sbatch_logs/stdout/k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-3_cross_entropy_%j.txt
-#SBATCH -e sbatch_logs/stderr/k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-3_cross_entropy_%j.err
+#SBATCH --job-name=k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy
+#SBATCH -o sbatch_logs/stdout/k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy_%j.txt
+#SBATCH -e sbatch_logs/stderr/k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy_%j.err
 #SBATCH --ntasks=1
 #SBATCH --partition=2080ti-long
 #SBATCH --gres=gpu:1
@@ -11,7 +11,7 @@
 eval "$(conda shell.bash hook)"
 conda activate qm
 
-EXPERIMENT_ID_PREFIX=k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-3_cross_entropy
+EXPERIMENT_ID_PREFIX=k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy
 EXPERIMENT_DATE=`date +"%m-%d"`
 
 python train.py --save_dir save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE} \
