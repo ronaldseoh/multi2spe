3ffbfc2a83f62d25f6b322dd77ce44c79ad659f0
Ronald Seoh <ronaldseoh@icloud.com>
2022-03-19 22:34:13 -0400

    Update check_scincl_specter_s2_mapping.py

--- snippets/check_scincl_specter_s2_mapping.py
+++ snippets/check_scincl_specter_s2_mapping.py
@@ -1,5 +1,7 @@
 import json
 import pickle
+import collections
+import statistics
 
 import tqdm
 
@@ -15,6 +17,8 @@ if __name__ == '__main__':
 
     citation_count = json.load(open('/gypsum/scratch1/bseoh/scincl_dataset/citation_count.json', 'r'))
 
+    mag_field = json.load(open('/gypsum/scratch1/bseoh/scincl_dataset/train_mag_field.json.json', 'r'))
+
     num_query_paper_ids_found = 0
     num_pos_paper_ids_found = 0
     num_neg_paper_ids_found = 0
@@ -23,8 +27,22 @@ if __name__ == '__main__':
     num_pos_paper_ids_found_cited = 0
     num_neg_paper_ids_found_cited = 0
 
+    query_paper_count_by_mag_field = collections.defaultdict(int)
+    pos_paper_count_by_mag_field = collections.defaultdict(int)
+    neg_paper_count_by_mag_field = collections.defaultdict(int)
+
+    num_query_paper_ids_found_mag = 0
+    num_pos_paper_ids_found_mag = 0
+    num_neg_paper_ids_found_mag = 0
+
     num_both_found = 0
     num_all_found = 0
+
+    num_both_found_cited = 0
+    num_all_found_cited = 0
+
+    num_both_found_mag = 0
+    num_all_found_mag = 0
 
     unique_paper_ids = set()
 
@@ -46,12 +64,28 @@ if __name__ == '__main__':
                 pos_paper_id_found = False
                 neg_paper_id_found = False
 
+                query_paper_id_found_cited = False
+                pos_paper_id_found_cited = False
+                neg_paper_id_found_cited = False
+
+                query_paper_id_found_mag = False
+                pos_paper_id_found_mag = False
+                neg_paper_id_found_mag = False
+
                 if len(query_paper_id) > 0 and query_paper_id in mapping.keys():
                     num_query_paper_ids_found += 1
                     query_paper_id_found = True
 
                     if query_paper_id in citation_count.keys():
                         num_query_paper_ids_found_cited += 1
+                        query_paper_id_found_cited = True
+
+                    if query_paper_id in mag_field.keys():
+                        num_query_paper_ids_found_mag += 1
+                        query_paper_id_found_mag = True
+
+                        for f in mag_field[query_paper_id]['mag_field_of_study']:
+                            query_paper_count_by_mag_field[f] += 1
 
                 if len(pos_paper_id) > 0 and pos_paper_id in mapping.keys():
                     num_pos_paper_ids_found += 1
@@ -59,6 +93,14 @@ if __name__ == '__main__':
 
                     if pos_paper_id in citation_count.keys():
                         num_pos_paper_ids_found_cited += 1
+                        pos_paper_id_found_cited = True
+
+                    if pos_paper_id in mag_field.keys():
+                        num_pos_paper_ids_found_mag += 1
+                        pos_paper_id_found_mag = True
+
+                        for f in mag_field[pos_paper_id]['mag_field_of_study']:
+                            pos_paper_count_by_mag_field[f] += 1
 
                 if len(neg_paper_id) > 0 and neg_paper_id in mapping.keys():
                     num_neg_paper_ids_found += 1
@@ -66,12 +108,33 @@ if __name__ == '__main__':
 
                     if neg_paper_id in citation_count.keys():
                         num_neg_paper_ids_found_cited += 1
+                        neg_paper_id_found_cited = True
+
+                    if neg_paper_id in mag_field.keys():
+                        num_neg_paper_ids_found_mag += 1
+                        neg_paper_id_found_mag = True
+
+                        for f in mag_field[neg_paper_id]['mag_field_of_study']:
+                            neg_paper_count_by_mag_field[f] += 1
 
                 if pos_paper_id_found and neg_paper_id_found:
                     num_both_found += 1
 
+                    if pos_paper_id_found_cited and neg_paper_id_found_cited:
+                        num_both_found_cited += 1
+
+                    if pos_paper_id_found_mag and neg_paper_id_found_mag:
+                        num_both_found_mag += 1
+
                     if query_paper_id_found:
                         num_all_found += 1
+
+                        if pos_paper_id_found_cited and neg_paper_id_found_cited and query_paper_id_found_cited:
+                            num_all_found_cited += 1
+
+                        if pos_paper_id_found_mag and neg_paper_id_found_mag and query_paper_id_found_mag:
+                            num_all_found_mag += 1
+
             except EOFError:
                 break
 
@@ -82,5 +145,12 @@ if __name__ == '__main__':
     print("num_query_paper_ids_found_cited", str(num_query_paper_ids_found_cited))
     print("num_pos_paper_ids_found_cited", str(num_pos_paper_ids_found_cited))
     print("num_neg_paper_ids_found_cited", str(num_neg_paper_ids_found_cited))
+    print("num_query_paper_ids_found_mag", str(num_query_paper_ids_found_mag))
+    print("num_pos_paper_ids_found_mag", str(num_pos_paper_ids_found_mag))
+    print("num_neg_paper_ids_found_mag", str(num_neg_paper_ids_found_mag))
     print("num_both_found", str(num_both_found))
     print("num_all_found", str(num_all_found))
+    print("num_both_found_cited", str(num_both_found_cited))
+    print("num_all_found_cited", str(num_all_found_cited))
+    print("num_both_found_mag", str(num_both_found_mag))
+    print("num_all_found_mag", str(num_all_found_mag))
