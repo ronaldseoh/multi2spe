6e9664e03d64144057ad118cb5674c0a8acb7779
Ronald Seoh <ronaldseoh@icloud.com>
2022-03-24 01:38:41 -0400

    Update count_specter_paper_appearance.py

--- snippets/count_specter_paper_appearance.py
+++ snippets/count_specter_paper_appearance.py
@@ -13,8 +13,8 @@ def generator():
 
 if __name__ == '__main__':
 
-    specter_data_file_path = "/home/bseoh/my_scratch/original_data/val.pkl"
-    occurrence_count = collections.defaultdict(int)
+    specter_data_file_path = "/home/bseoh/my_scratch/original_data/train.pkl"
+    popularity_count = collections.defaultdict(int)
 
     with open(specter_data_file_path, 'rb') as f_in:
         unpickler = pickle.Unpickler(f_in)
@@ -26,8 +26,8 @@ if __name__ == '__main__':
                 pos_paper_id = instance.fields.get('pos_paper_id').metadata
                 neg_paper_id = instance.fields.get('neg_paper_id').metadata
 
-                occurrence_count[pos_paper_id] += 1
-                occurrence_count[neg_paper_id] += 1
+                popularity_count[pos_paper_id] += 1
+                popularity_count[neg_paper_id] += 1
 
             except EOFError:
                 break
@@ -55,12 +55,12 @@ if __name__ == '__main__':
 
                 if not avg_pos_weights_ids_seen[pos_paper_id]:
                     avg_pos_weights_num_seen += 1
-                    avg_pos_weights += 1 / occurrence_count[pos_paper_id]
+                    avg_pos_weights += 1 / popularity_count[pos_paper_id]
                     avg_pos_weights_ids_seen[pos_paper_id] = True
 
                 if not avg_neg_weights_ids_seen[neg_paper_id]:
                     avg_neg_weights_num_seen += 1
-                    avg_neg_weights += 1 / occurrence_count[neg_paper_id]
+                    avg_neg_weights += 1 / popularity_count[neg_paper_id]
                     avg_neg_weights_ids_seen[neg_paper_id] = True
 
             except EOFError:
@@ -77,14 +77,15 @@ if __name__ == '__main__':
     avg_weights['avg_neg_weights'] = avg_neg_weights
     avg_weights['avg_pos_neg_weights'] = avg_pos_neg_weights
 
-    with open('/home/bseoh/my_scratch/original_data/val_popularity_count.json', 'w') as popularity_count_file:
-        json.dump(occurrence_count, popularity_count_file)
 
-    with open('/home/bseoh/my_scratch/original_data/val_avg_weights.json', 'w') as avg_weights_file:
+    with open('/home/bseoh/my_scratch/original_data/train_popularity_count.json', 'w') as popularity_count_file:
+        json.dump(popularity_count, popularity_count_file)
+
+    with open('/home/bseoh/my_scratch/original_data/train_avg_weights.json', 'w') as avg_weights_file:
         json.dump(avg_weights, avg_weights_file)
 
-    print("Max occurrence:", str(max(occurrence_count.values())))
-    print("Min occurrence:", str(min(occurrence_count.values())))
-    print("Mean occurrence:", str(statistics.mean(occurrence_count.values())))
-    print("Median occurrence:", str(statistics.median(occurrence_count.values())))
+    print("Max occurrence:", str(max(popularity_count.values())))
+    print("Min occurrence:", str(min(popularity_count.values())))
+    print("Mean occurrence:", str(statistics.mean(popularity_count.values())))
+    print("Median occurrence:", str(statistics.median(popularity_count.values())))
     print("avg_weights:", str(avg_weights))
