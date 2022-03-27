9c48876d647155cd61fab419e09985ca1e252a8c
Ronald Seoh <ronaldseoh@icloud.com>
2022-03-27 14:24:03 -0400

    Update scidocs_only.sh

--- sbatch/scidocs_only.sh
+++ sbatch/scidocs_only.sh
@@ -3,7 +3,8 @@
 #SBATCH -o sbatch_logs/stdout/scidocs_only_%j.txt
 #SBATCH -e sbatch_logs/stderr/scidocs_only_%j.err
 #SBATCH --ntasks=1
-#SBATCH --partition=1080ti-short
+#SBATCH --partition=gpu
+#SBATCH --constraint=ials_gigabyte_gpu_2020
 #SBATCH --gres=gpu:1
 #SBATCH --mem=24GB
 #SBATCH --cpus-per-task=2
@@ -11,8 +12,8 @@
 eval "$(conda shell.bash hook)"
 conda activate scidocs
 
-EXPERIMENT_ID_PREFIX=k-3_separate_layer_8_4_identity_nsp_cross_entropy
-EXPERIMENT_DATE="10-14"
+EXPERIMENT_ID_PREFIX=U_k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_margin
+EXPERIMENT_DATE="03-25"
 
 python ../scidocs/scripts/run.py --cls save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/cls.jsonl \
                       --user-citation save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation.jsonl \
