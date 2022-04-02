d2284986c386eb27c76c37d0f5bbefa9c1aa0f63
Ronald Seoh <ronaldseoh@icloud.com>
2022-04-02 17:36:11 -0400

    Update scidocs_only_temp1228_with_embed_disable_sum.sh

--- sbatch/scidocs_only_temp1228_with_embed_disable_sum.sh
+++ sbatch/scidocs_only_temp1228_with_embed_disable_sum.sh
@@ -3,8 +3,7 @@
 #SBATCH -o sbatch_logs/stdout/scidocs_only_cosine_%j.txt
 #SBATCH -e sbatch_logs/stderr/scidocs_only_cosine_%j.err
 #SBATCH --ntasks=1
-#SBATCH --partition=gpu
-#SBATCH --constraint=ials_gigabyte_gpu_2020
+#SBATCH --partition=1080ti-short
 #SBATCH --gres=gpu:1
 #SBATCH --mem=24GB
 #SBATCH --cpus-per-task=2
@@ -12,8 +11,8 @@
 eval "$(conda shell.bash hook)"
 conda activate qm
 
-EXPERIMENT_ID_PREFIX=scincl_U_k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4_identity_common_random_cross_entropy
-EXPERIMENT_DATE="02-26"
+EXPERIMENT_ID_PREFIX=shard11_k-3_debug_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-v5_cross_entropy
+EXPERIMENT_DATE="03-30"
 
 python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
                 --data-path ../scidocs/data/paper_metadata_mag_mesh.json \
