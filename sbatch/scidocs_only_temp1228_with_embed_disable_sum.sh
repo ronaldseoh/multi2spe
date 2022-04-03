842060e0c4a3e26d0326ad0d1a56596d904ae89a
Ronald Seoh <ronaldseoh@icloud.com>
2022-04-02 22:46:00 -0400

    Update scidocs_only_temp1228_with_embed_disable_sum.sh

--- sbatch/scidocs_only_temp1228_with_embed_disable_sum.sh
+++ sbatch/scidocs_only_temp1228_with_embed_disable_sum.sh
@@ -11,7 +11,7 @@
 eval "$(conda shell.bash hook)"
 conda activate qm
 
-EXPERIMENT_ID_PREFIX=shard11_k-3_debug_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-v5_cross_entropy
+EXPERIMENT_ID_PREFIX=shard11_k-3_debug_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_cross_entropy
 EXPERIMENT_DATE="03-30"
 
 python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
