65c4b7a07ea8ba2fc63dcb9d5cba620945aa97c3
Ronald Seoh <ronaldseoh@icloud.com>
2022-04-20 14:46:05 -0400

    Update custom_cite.sh

--- sbatch/custom_cite.sh
+++ sbatch/custom_cite.sh
@@ -11,8 +11,8 @@
 eval "$(conda shell.bash hook)"
 conda activate qm
 
-EXPERIMENT_ID_PREFIX=scincl_specter
-EXPERIMENT_DATE="03-10"
+EXPERIMENT_ID_PREFIX=shard11_k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-v5_cross_entropy
+EXPERIMENT_DATE="03-30"
                 
 python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
                 --data-path ~/my_scratch/scidocs-shard7/data_final.json \
@@ -25,5 +25,6 @@ python ../scidocs/scripts/run_custom_cite.py --user-citation ../quartermaster/sa
                       --val_or_test test \
                       --multifacet-behavior extra_linear \
                       --n-jobs 4 --cuda-device 0 \
+                      --user-citation-metric "cosine" \
                       --data-path ~/my_scratch/scidocs-shard7 \
-                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_custom_cite.xlsx
+                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_cosine_custom_cite.xlsx
