cf68bdf4ec77e2856f9291e0c4c841a0831bdd11
Ronald Seoh <ronaldseoh@icloud.com>
2022-04-22 16:24:37 -0400

    Update custom_cite.sh

--- sbatch/custom_cite.sh
+++ sbatch/custom_cite.sh
@@ -10,8 +10,8 @@
 
 eval "$(conda shell.bash hook)"
 
-EXPERIMENT_ID_PREFIX=k-3_sum_embs_original-0-5+no_sum-0-5+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy
-EXPERIMENT_DATE="03-21"
+EXPERIMENT_ID_PREFIX=scincl_specter
+EXPERIMENT_DATE="03-10"
 
 conda activate scidocs
 
@@ -19,6 +19,5 @@ python ../scidocs/scripts/run_custom_cite.py --user-citation ../quartermaster/sa
                       --val_or_test test \
                       --multifacet-behavior extra_linear \
                       --n-jobs 4 --cuda-device 0 \
-                      --user-citation-metric "cosine" \
                       --data-path ~/my_scratch/scidocs-shard7 \
-                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_cosine_custom_cite.xlsx
+                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_custom_cite.xlsx
