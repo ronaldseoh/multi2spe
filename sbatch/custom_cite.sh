6ef4073889004c21f50d3a13663b4c1a0d81ce00
Ronald Seoh <ronaldseoh@icloud.com>
2022-04-22 07:43:18 -0400

    Update custom_cite.sh

--- sbatch/custom_cite.sh
+++ sbatch/custom_cite.sh
@@ -11,8 +11,8 @@
 eval "$(conda shell.bash hook)"
 conda activate qm
 
-EXPERIMENT_ID_PREFIX=k-3_sum_embs_original-0-5+no_sum-0-5+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy
-EXPERIMENT_DATE="03-21"
+EXPERIMENT_ID_PREFIX=scincl_k-3_sum_embs_original-0-5+no_sum-0-5_extra_facet_alternate_layer_8_4_identity_common_random_cross_entropy
+EXPERIMENT_DATE="03-17"
                 
 python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
                 --data-path ~/my_scratch/scidocs-shard7/data_final.json \
