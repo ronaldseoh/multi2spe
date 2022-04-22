f57ecfa7d1c90dd07c9c080b34535722aabd8fdb
Ronald Seoh <ronaldseoh@icloud.com>
2022-04-22 08:24:55 -0400

    Revert "Update custom_cite_unity.sh"
    This reverts commit 146e5d97a473d02ce08818bbd2b432609ce1f3c8.

--- sbatch/custom_cite_unity.sh
+++ sbatch/custom_cite_unity.sh
@@ -12,10 +12,10 @@
 eval "$(conda shell.bash hook)"
 conda activate qm
 
-EXPERIMENT_ID_PREFIX=specter
-EXPERIMENT_DATE="09-09"
+EXPERIMENT_ID_PREFIX=U_k-3_sum_embs_original-0-5+no_sum-0-5+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights_cross_entropy
+EXPERIMENT_DATE="03-23"
                 
-python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
+python embed.py --pl-checkpoint-path /old/scratch/bseoh_umass_edu/quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
                 --data-path /gypsum/scratch1/bseoh/scidocs-shard7/data_final.json \
                 --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation_custom_cite.jsonl --batch-size 4
 
@@ -26,5 +26,6 @@ python ../scidocs/scripts/run_custom_cite.py --user-citation ../quartermaster/sa
                       --val_or_test test \
                       --multifacet-behavior extra_linear \
                       --n-jobs 4 --cuda-device 0 \
+                      --user-citation-metric "cosine" \
                       --data-path /gypsum/scratch1/bseoh/scidocs-shard7 \
-                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_custom_cite.xlsx
+                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_cosine_custom_cite.xlsx
