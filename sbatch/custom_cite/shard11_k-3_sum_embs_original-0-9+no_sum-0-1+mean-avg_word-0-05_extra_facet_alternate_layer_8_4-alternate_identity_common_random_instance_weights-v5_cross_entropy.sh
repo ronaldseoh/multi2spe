710f49fe956ceffb3e7d5accbacfadcabb972dcc
Ronald Seoh <ronaldseoh@icloud.com>
2022-04-23 07:28:23 -0400

    sbatch/custom_cite: add back the embedding step

--- sbatch/custom_cite/shard11_k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-v5_cross_entropy.sh
+++ sbatch/custom_cite/shard11_k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-v5_cross_entropy.sh
@@ -9,10 +9,16 @@
 #SBATCH --cpus-per-task=2
 
 eval "$(conda shell.bash hook)"
+conda activate qm
 
 EXPERIMENT_ID_PREFIX=shard11_k-3_sum_embs_original-0-9+no_sum-0-1+mean-avg_word-0-05_extra_facet_alternate_layer_8_4-alternate_identity_common_random_instance_weights-v5_cross_entropy
 EXPERIMENT_DATE="03-30"
 
+python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
+                --data-path ~/my_scratch/scidocs-shard7-14/data_final.json \
+                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation_custom_cite.jsonl --batch-size 4
+
+conda deactivate
 conda activate scidocs
 
 python ../scidocs/scripts/run_custom_cite.py --user-citation ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation_custom_cite.jsonl \
--- sbatch/custom_cite/shard11_specter.sh
+++ sbatch/custom_cite/shard11_specter.sh
@@ -9,10 +9,16 @@
 #SBATCH --cpus-per-task=2
 
 eval "$(conda shell.bash hook)"
+conda activate qm
 
 EXPERIMENT_ID_PREFIX=shard11_specter
 EXPERIMENT_DATE="03-28"
 
+python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
+                --data-path ~/my_scratch/scidocs-shard7-14/data_final.json \
+                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation_custom_cite.jsonl --batch-size 4
+
+conda deactivate
 conda activate scidocs
 
 python ../scidocs/scripts/run_custom_cite.py --user-citation ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation_custom_cite.jsonl \
