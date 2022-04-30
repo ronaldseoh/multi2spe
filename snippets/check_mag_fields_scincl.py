b619dbb6db377640bae987afeb0fe31275fe5d48
Ronald Seoh <ronaldseoh@icloud.com>
2022-04-30 15:56:15 -0400

    Update check_mag_fields_scincl.py

--- snippets/check_mag_fields_scincl.py
+++ snippets/check_mag_fields_scincl.py
@@ -1,14 +1,9 @@
 import json
-import pickle
+import csv
 import collections
 import statistics
 
 import tqdm
-
-
-def generator():
-    while True:
-        yield
 
 
 if __name__ == '__main__':
@@ -25,37 +20,48 @@ if __name__ == '__main__':
 
     extra_metadata = json.load(open('preprocessed/train_extra_metadata.json', 'r'))
 
-    with open("preprocessed/data-train.p", 'rb') as f_in:
-        unpickler = pickle.Unpickler(f_in)
-        for _ in tqdm.tqdm(generator()):
-            try:
-                instance = unpickler.load()
+    with open('train_triples.csv', 'r') as f_in:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in tqdm.tqdm(reader):
+            query_paper_id = row[0]
+            pos_paper_id = row[1]
+            neg_paper_id = row[2]
 
-                query_paper_id = instance.fields.get('source_paper_id').metadata
-                pos_paper_id = instance.fields.get('pos_paper_id').metadata
-                neg_paper_id = instance.fields.get('neg_paper_id').metadata
+            unique_paper_ids.add(query_paper_id)
+            unique_paper_ids.add(pos_paper_id)
+            unique_paper_ids.add(neg_paper_id)
 
-                unique_paper_ids.add(query_paper_id)
-                unique_paper_ids.add(pos_paper_id)
-                unique_paper_ids.add(neg_paper_id)
+            if query_paper_id in extra_metadata.keys() and extra_metadata[query_paper_id]['mag_field_of_study'] is not None:
+                num_query_paper_ids_found_mag += 1
 
-                if query_paper_id in extra_metadata.keys() and extra_metadata[query_paper_id]['mag_field_of_study'] is not None:
-                    num_query_paper_ids_found_mag += 1
+                for f in extra_metadata[query_paper_id]['mag_field_of_study']:
+                    query_paper_count_by_mag_field[f] += 1
 
-                    for f in extra_metadata[query_paper_id]['mag_field_of_study']:
-                        query_paper_count_by_mag_field[f] += 1
+            if pos_paper_id in extra_metadata.keys() and extra_metadata[pos_paper_id]['mag_field_of_study'] is not None:
+                num_pos_paper_ids_found_mag += 1
 
-                if pos_paper_id in extra_metadata.keys() and extra_metadata[pos_paper_id]['mag_field_of_study'] is not None:
-                    num_pos_paper_ids_found_mag += 1
+                for f in extra_metadata[pos_paper_id]['mag_field_of_study']:
+                    pos_paper_count_by_mag_field[f] += 1
 
-                    for f in extra_metadata[pos_paper_id]['mag_field_of_study']:
-                        pos_paper_count_by_mag_field[f] += 1
+            if neg_paper_id in extra_metadata.keys() and extra_metadata[neg_paper_id]['mag_field_of_study'] is not None:
+                num_neg_paper_ids_found_mag += 1
 
-                if neg_paper_id in extra_metadata.keys() and extra_metadata[neg_paper_id]['mag_field_of_study'] is not None:
-                    num_neg_paper_ids_found_mag += 1
+                for f in extra_metadata[neg_paper_id]['mag_field_of_study']:
+                    neg_paper_count_by_mag_field[f] += 1
 
-                    for f in extra_metadata[neg_paper_id]['mag_field_of_study']:
-                        neg_paper_count_by_mag_field[f] += 1
+    print('num_query_paper_ids_found_mag=', str(num_query_paper_ids_found_mag))
+    print('num_pos_paper_ids_found_mag=', str(num_pos_paper_ids_found_mag))
+    print('num_neg_paper_ids_found_mag=', str(num_neg_paper_ids_found_mag))
+    print()
 
-            except EOFError:
-                break
+    print('query_paper_count_by_mag_field')
+    print(query_paper_count_by_mag_field)
+    print()
+
+    print('pos_paper_count_by_mag_field')
+    print(pos_paper_count_by_mag_field)
+    print()
+
+    print('neg_paper_count_by_mag_field')
+    print(neg_paper_count_by_mag_field)
+    print()
