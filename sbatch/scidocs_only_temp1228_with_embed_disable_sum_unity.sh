b1ffefdde60d291087f274fd58ceff6ee0d3532f
Ronald Seoh <ronaldseoh@icloud.com>
2022-05-15 10:22:29 -0400

    Create scidocs_only_temp1228_with_embed_disable_sum_unity.sh

---
+++ sbatch/scidocs_only_temp1228_with_embed_disable_sum_unity.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=scidocs_only_cosine
+#SBATCH -o sbatch_logs/stdout/scidocs_only_cosine_%j.txt
+#SBATCH -e sbatch_logs/stderr/scidocs_only_cosine_%j.err
+#SBATCH --ntasks=1
+#SBATCH --partition=gpu-long
+#SBATCH --constraint=ials_gigabyte_gpu_2020
+#SBATCH --gres=gpu:1
+#SBATCH --mem=24GB
+#SBATCH --cpus-per-task=2
+
+eval "$(conda shell.bash hook)"
+conda activate qm
+
+EXPERIMENT_ID_PREFIX=scincl_U_k-3_sum_embs_original-0-9+no_sum-0-1_extra_facet_alternate_layer_8_4-alternate_identity_common_random_cross_entropy
+EXPERIMENT_DATE="05-04"
+
+python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
+                --data-path ../scidocs/data/paper_metadata_mag_mesh.json \
+                --output save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/cls_no_sum.jsonl --batch-size 4 \
+                --debug_disable_sum_embs
