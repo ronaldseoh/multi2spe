2593a0abcc744350f93b4c0c963ea67e31cf2330
Ronald Seoh <ronaldseoh@icloud.com>
2022-05-17 17:59:47 -0400

    Update save_facet_magnitudes.py

--- snippets/save_facet_magnitudes.py
+++ snippets/save_facet_magnitudes.py
@@ -38,16 +38,44 @@ for f in range(num_facets):
 
 output_dict['field'] = []
 
+embeddings_by_mag_field = {}
+mean_embeddings_by_mag_field = {}
+
+for f in range(num_facets):
+    embeddings_by_mag_field[f] = {}
+    mean_embeddings_by_mag_field[f] = {}
+
+    for fn in mag_label_mapping.keys():
+        embeddings_by_mag_field[f][fn] = []
+
 with open('cls_no_sum.jsonl', 'r') as mag_embeddings_file:
     for line in tqdm.tqdm(mag_embeddings_file):
         paper = json.loads(line)
 
         if paper["paper_id"] in mag_val_pids:
+            mag_field_num = mag_val[mag_val.pid == paper["paper_id"]].iloc[0].class_label
+
             for f in range(num_facets):
                 emb = paper["embedding"][f]
-                output_dict['magnitude_{}'.format(f)].append(np.linalg.norm(emb))
+                embeddings_by_mag_field[f][mag_field_num].append(emb)
 
-            output_dict['field'].append(mag_label_mapping[mag_val[mag_val.pid == paper["paper_id"]].iloc[0].class_label])
+for f in range(num_facets):
+    for fn in mag_label_mapping.keys():
+        mean_embeddings_by_mag_field[f][fn] = np.mean(np.array(embeddings_by_mag_field[f][fn]), axis=0)
+
+with open('cls_no_sum.jsonl', 'r') as mag_embeddings_file:
+    for line in tqdm.tqdm(mag_embeddings_file):
+        paper = json.loads(line)
+
+        if paper["paper_id"] in mag_val_pids:
+            mag_field_num = mag_val[mag_val.pid == paper["paper_id"]].iloc[0].class_label
+            mag_field = mag_label_mapping[mag_field_num]
+
+            output_dict['field'].append(mag_field)
+
+            for f in range(num_facets):
+                emb = paper["embedding"][f]
+                output_dict['magnitude_{}'.format(f)].append(np.linalg.norm(emb - mean_embeddings_by_mag_field[f][mag_field_num]))
             
 df = pd.DataFrame.from_dict(output_dict)
 df.to_excel('magnitudes.xlsx')
