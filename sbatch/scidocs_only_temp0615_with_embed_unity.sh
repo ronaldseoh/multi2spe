56dd8f7e30afed9258ecab9fa49e32f2f89ed671
Ronald Seoh <ronaldseoh@icloud.com>
2022-06-19 12:49:49 +0900

    up

--- sbatch/scidocs_only_temp0615_with_embed_unity.sh
+++ sbatch/scidocs_only_temp0615_with_embed_unity.sh
@@ -12,7 +12,7 @@
 eval "$(conda shell.bash hook)"
 conda activate qm
 
-EXPERIMENT_ID_PREFIX=shard11_U_k-3_sum_embs_original-0+no_sum-1_extra_facet_alternate_layer_8_4-alternate_identity_common_random_cross_entropy
+EXPERIMENT_ID_PREFIX=k-3_debug_sum_embs_original-0+no_sum-1_extra_facet_alternate_layer_8_4-alternate_identity_common_random_cross_entropy
 EXPERIMENT_DATE="06-13"
 
 python embed.py --pl-checkpoint-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/checkpoints/last.ckpt \
