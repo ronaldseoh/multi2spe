6709fc6bca1dfbb7ef94ac684677418891f08a4a
Ronald Seoh <ronaldseoh@icloud.com>
2022-06-20 19:49:27 +0900

    up

--- sbatch/unity/custom_cite/no_embed/scincl_U_specter.sh
+++ sbatch/unity/custom_cite/no_embed/scincl_U_specter.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#SBATCH -A pi_tbernard_umass_edu
 #SBATCH --job-name=custom_cite_scincl_U_specter
 #SBATCH -o sbatch_logs/stdout/custom_cite_scincl_U_specter_%j.txt
 #SBATCH -e sbatch_logs/stderr/custom_cite_scincl_U_specter_%j.err
 #SBATCH --ntasks=1
-#SBATCH --partition=cpu-long
+#SBATCH --partition=gypsum-1080ti-ms
 #SBATCH --mem=40GB
 #SBATCH --cpus-per-task=2
 
@@ -19,4 +20,11 @@ python ../scidocs/scripts/run_custom_cite.py --user-citation ../quartermaster/sa
                       --multifacet-behavior extra_linear \
                       --n-jobs 4 --cuda-device 0 \
                       --data-path /gypsum/scratch1/bseoh/scidocs-shard7 \
-                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_custom_cite_cross_domain.xlsx
+                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_custom_cite.xlsx
+
+python ../scidocs/scripts/run_custom_cite.py --user-citation ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation_custom_cite.jsonl \
+                      --val_or_test test \
+                      --multifacet-behavior extra_linear \
+                      --n-jobs 4 --cuda-device 0 \
+                      --data-path /gypsum/scratch1/bseoh/scidocs-shard7-full \
+                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_custom_cite-full.xlsx
