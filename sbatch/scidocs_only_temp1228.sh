3e157abef9f76cda638f63bfca4d520a927f9dbb
Ronald Seoh <ronaldseoh@icloud.com>
2022-07-15 10:06:00 +0900

    up

--- sbatch/scidocs_only_temp1228.sh
+++ sbatch/scidocs_only_temp1228.sh
@@ -5,34 +5,21 @@
 #SBATCH --ntasks=1
 #SBATCH --partition=gpu
 #SBATCH --constraint=ials_gigabyte_gpu_2020
-#SBATCH --gres=gpu:1
 #SBATCH --mem=24GB
 #SBATCH --cpus-per-task=2
 
 eval "$(conda shell.bash hook)"
 
 
-EXPERIMENT_ID_PREFIX=scincl_U_k-3_normalized_magnitudes_sum_embs_original-0-5+no_sum-0-5+mean-avg_word-0-05_extra_facet_alternate_layer_8_4_identity_common_random_margin
-EXPERIMENT_DATE="04-16"
+EXPERIMENT_ID_PREFIX=shard11_U_k-3_sum_embs_original-0-9+no_sum-0-1_extra_facet_alternate_layer_8_4-alternate_identity_common_random_cross_entropy
+EXPERIMENT_DATE="07-14"
 
 conda activate scidocs
 
-python ../scidocs/scripts/run.py --cls save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/cls.jsonl \
-                      --user-citation save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation.jsonl \
-                      --recomm save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/recomm.jsonl \
+python ../scidocs/scripts/run_custom_cite.py --user-citation save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation_custom_cite.jsonl \
                       --val_or_test test \
                       --multifacet-behavior extra_linear \
                       --n-jobs 4 --cuda-device 0 \
-                      --cls-svm \
-                      --data-path ../scidocs/data \
-                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results.xlsx
-
-python ../scidocs/scripts/run.py --cls ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/cls_no_multiply_magnitudes.jsonl \
-                      --user-citation ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/user-citation_no_multiply_magnitudes.jsonl \
-                      --recomm ../quartermaster/save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/recomm_no_multiply_magnitudes.jsonl \
-                      --val_or_test test \
-                      --multifacet-behavior extra_linear \
-                      --n-jobs 4 --cuda-device 0 \
-                      --cls-svm \
-                      --data-path ../scidocs/data \
-                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_no_multiply_magnitudes.xlsx
+                      --user-citation-metric "cosine" \
+                      --data-path /gypsum/scratch1/bseoh/scidocs-shard7-full \
+                      --results-save-path save_${EXPERIMENT_ID_PREFIX}_${EXPERIMENT_DATE}/results_cosine_custom_cite-full.xlsx
