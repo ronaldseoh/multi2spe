159f444a0d683def91569721d785d22bc13df55c
Ronald Seoh <ronaldseoh@icloud.com>
2022-07-15 10:07:08 +0900

    up

--- sbatch/scidocs_only_temp1228.sh
+++ sbatch/scidocs_only_temp1228.sh
@@ -3,7 +3,7 @@
 #SBATCH -o sbatch_logs/stdout/scidocs_only_cosine_%j.txt
 #SBATCH -e sbatch_logs/stderr/scidocs_only_cosine_%j.err
 #SBATCH --ntasks=1
-#SBATCH --partition=gpu
+#SBATCH --partition=gpu-long
 #SBATCH --constraint=ials_gigabyte_gpu_2020
 #SBATCH --mem=24GB
 #SBATCH --cpus-per-task=2
