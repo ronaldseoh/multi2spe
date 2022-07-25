793e43c6deebf0aa02223cc3355ad8da96bcbfb7
Ronald Seoh <ronaldseoh@icloud.com>
2022-07-26 08:40:38 +0900

    wip

--- snippets/visualize_embeddings_titles_with_s2orc_query_papers.py
+++ snippets/visualize_embeddings_titles_with_s2orc_query_papers.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
 
     mag_labels = []
 
-    with open("quartermaster/save_shard11_U_k-3_sum_embs_original-0-9+no_sum-0-1_extra_facet_alternate_layer_8_4-alternate_identity_common_random_cross_entropy_05-04/cls_no_sum.jsonl", "r") as mag_embeddings_file:
+    with open("quartermaster/save_U_k-3_sum_embs_original-0-9+no_sum-0-1_extra_facet_alternate_layer_8_4-alternate_identity_common_random_cross_entropy_05-04/cls_no_sum.jsonl", "r") as mag_embeddings_file:
         for line in tqdm.tqdm(mag_embeddings_file):
             paper = json.loads(line)
 
